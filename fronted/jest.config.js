85ae9747e9798b71e9ee5730327e456909608e76
CJ-Danny <1135878957@qq.com>
2025-03-03 07:35:01 +0800

    update test

--- fronted/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    preset: 'jest-preset-vue',
-    transform: {
-        '^.+\\.vue$': 'vue-jest',
-        '^.+\\.js$': 'babel-jest'
-    },
-    testEnvironment: 'jsdom',
-    moduleFileExtensions: ['js', 'vue', 'json'],
-}
