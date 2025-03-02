7a2982503c46b574aff08a1cc5b4626d344644ef
CJ-Danny <1135878957@qq.com>
2025-03-03 04:42:25 +0800

    deploy fronted

--- fronted/vite.config.js
+++ fronted/vite.config.js
@@ -4,4 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: '0.0.0.0', // 让应用可通过外部访问
+    port: 8080,       // 可以根据需要修改端口
+  }
 })
