10957212dbf92e92df5d0b4e90415121474d7c15
AlexZhao <wrzhao16@gmail.com>
2025-03-02 21:52:22 -0500

    config

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,26 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import * as echarts from 'echarts';
+import axios from 'axios';
+import locale from 'element-ui/lib/locale/lang/en'
 
-createApp(App).mount("#app");
+Vue.use(ElementUI, { locale })
+
+Vue.config.productionTip = false
+
+Vue.use(ElementUI);
+
+Vue.prototype.$axios = axios;
+Vue.prototype.$echarts = echarts;
+
+axios.defaults.baseURL = 'http://15.157.62.32:8000/api/';
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
