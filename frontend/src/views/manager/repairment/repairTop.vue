b499ff61e8577729452ea51eb6493ca4eefcec2c
AlexZhao <wrzhao16@gmail.com>
2025-03-19 16:36:19 -0400

    create repair top index

---
+++ frontend/src/views/manager/repairment/repairTop.vue
@@ -0,0 +1,21 @@
+<template>
+  <TopIndex2 :data="dt" :type="4"></TopIndex2>
+</template>
+
+<script>
+import TopIndex2 from "@/components/TopIndex2";
+export default {
+  name: "repairTop",
+  components: { TopIndex2 },
+  data() {
+    return {
+      dt: [
+        {src: require('@/assets/管理员端/statistics/icon0.png'), title: 'Pending maintenance orders'},
+        {src: require('@/assets/管理员端/statistics/icon1.png'), title: 'Completed maintenance orders'}
+      ],
+    }
+  },
+}
+</script>
+<style scoped>
+</style>
