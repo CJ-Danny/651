0f2e28c5dde2347fb192d9762020574ccba781fd
CJ-Danny <1135878957@qq.com>
2025-03-29 04:29:05 +0800

    GetManagerOrderTest

--- backend/service/tests.py
+++ backend/service/tests.py
@@ -104,3 +104,86 @@ class AssainOrderTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json()['errno'], 1000)
         self.assertEqual(response.json()['msg'], "wrong method")
+
+
+class GetManagerOrderTest(TestCase):
+    def setUp(self):
+        self.client = self.client_class()
+
+        self.manager = User.objects.create(
+            userId=456,
+            userName="manager",
+            password="test",
+            email="manager@gmail.com"
+        )
+
+        self.user = User.objects.create(
+            userId=123,
+            userName="danny",
+            password="test",
+            email="test@gmail.com"
+        )
+
+        self.order1 = Order.objects.create(
+            userID=self.user.userId,
+            roomID=101,
+            description="Order 1 description",
+            submitTime=timezone.now(),
+            assignTime=timezone.now(),
+            finishTime=timezone.now(),
+            status=1,
+            managerID=self.manager.userId
+        )
+
+        self.order2 = Order.objects.create(
+            userID=self.user.userId,
+            roomID=102,
+            description="Order 2 description",
+            submitTime=timezone.now(),
+            assignTime=timezone.now(),
+            finishTime=timezone.now(),
+            status=1,
+            managerID=self.manager.userId
+        )
+
+        self.order3 = Order.objects.create(
+            userID=self.user.userId,
+            roomID=103,
+            description="Order 3 description",
+            submitTime=timezone.now(),
+            assignTime=timezone.now(),
+            finishTime=timezone.now(),
+            status=0,
+            managerID=self.user.userId
+        )
+
+    def test_get_manager_orders_success(self):
+        response = self.client.post(reverse('getManagerOrder'), {
+            'managerID': self.manager.userId
+        })
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()['errno'], 0)
+        self.assertEqual(len(response.json()['data']), 2)
+
+        order_ids = [order['orderID'] for order in response.json()['data']]
+        self.assertIn(self.order1.orderID, order_ids)
+        self.assertIn(self.order2.orderID, order_ids)
+
+    def test_get_manager_orders_wrong_method(self):
+        response = self.client.get(reverse('getManagerOrder'), {
+            'managerID': self.manager.userId
+        })
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()['errno'], 1000)
+        self.assertEqual(response.json()['msg'], "wrong method")
+
+    def test_get_manager_orders_no_orders(self):
+        response = self.client.post(reverse('getManagerOrder'), {
+            'managerID': 9999
+        })
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()['errno'], 0)
+        self.assertEqual(response.json()['data'], [])
